78c91fd6644d622bbdef73c8a593bbf3ce5aaaa8
Lucas Kingeski <amongerebuzzz@gmail.com>
2025-09-05 10:31:05 -0300

    Versão Módulo http

--- nodejs/modulos/http/app.js
+++ nodejs/modulos/http/app.js
@@ -5,3 +5,4 @@ http.createServer((req, res) => {// Função de callback que lida com as requisi
   res.writeHead(200, {'Content-Type': 'text/plain'});// Define o cabeçalho da resposta com status 200 e tipo de conteúdo texto simples
   res.end('Hello World!');// Envia a resposta "Hello World!" e finaliza a resposta
 }).listen(8080);// O servidor escuta na porta 8080
+console.log('Servidor rodando em http://localhost:8080/');// Loga uma mensagem indicando que o servidor está rodando
