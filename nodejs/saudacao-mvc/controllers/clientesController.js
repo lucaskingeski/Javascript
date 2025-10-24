e4edb3101c54076491025984d71e7836f88af948
Lucas Kingeski <amongerebuzzz@gmail.com>
2025-10-24 09:24:53 -0300

    Versão com login de clientes funcionando e verificando

--- nodejs/saudacao-mvc/controllers/clientesController.js
+++ nodejs/saudacao-mvc/controllers/clientesController.js
@@ -1,8 +1,15 @@
+const loginModel = require('../models/loginModel');
+
 module.exports = {
     clientes: (req, res) => {
         res.sendFile('clientes.html', { root: './views' });
     },
     login : (req, res) => {
         res.sendFile('login.html', { root: './views' });
+    },
+    autenticar: (req, res) => {
+        const { email, senha } = req.body;
+        const mensagem = loginModel.autenticarUsuario(email, senha);
+        res.send(`<h1>${mensagem}</h1>`);
     }
 };
--- nodejs/saudacao-mvc/routes/clientes.js
+++ nodejs/saudacao-mvc/routes/clientes.js
@@ -4,5 +4,6 @@ const clientesController = require('../controllers/clientesController');
 
 router.get('/', clientesController.clientes);
 router.get('/login', clientesController.login);
+router.post('/autenticar', clientesController.autenticar);
 
 module.exports = router;
